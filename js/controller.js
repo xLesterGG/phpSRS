a8476565546c25e288259a05aba08e29ff487d36
xLesterGG <lesgoh@hotmail.com>
2016-09-28 19:19:31 +0800

    implement sales addition module

--- js/controller.js
+++ js/controller.js
@@ -5,7 +5,8 @@ var app = angular.module("myApp", ['ui.router']);
 
 app.controller("myCtrl", function ($scope, $http) {
 	'use strict';
-	$scope.invenMode = 'add';
+    
+    $scope.invenMode = 'add';
     $scope.salesMode = 'add';
 	
     $scope.chngInvenMode  = function () {
@@ -15,6 +16,46 @@ app.controller("myCtrl", function ($scope, $http) {
     $scope.chngSalesMode  = function () {
         $scope.salesMode = 'edit';
     };
+    
+   
+   
+	
+    
+    
+});
+
+app.controller("salesCtrl",function($scope){
+    'use strict';
+    
+     $scope.sales= [
+        {"sID":"1", itemName:"Pills", "itemUnit":"1", "clientName":"????", "clientContact":"123","uID":"1","sDate":"11-11-1111"}
+    ];
+    
+
+    
+   
+    $scope.addSales = function (itemName,itemUnit,clientName,clientContact,sDate) {
+        
+    $scope.currentID = parseInt($scope.sales[$scope.sales.length-1].sID) + 1;
+
+       var input = {"sID": $scope.currentID,
+                 itemName: itemName,
+                 itemUnit:itemUnit,
+                 clientName:clientName,
+                 clientContact:clientContact,
+                 uID:"1",
+                 sDate:sDate };       
+
+        $scope.sales.push(input);      
+
+
+        $scope.itemName ="";
+       
+   };
+    
+    
+    
+    
 });
 
 
